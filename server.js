5de952f97a2e45e44f659740d350dd691e7d4210
lennartdeknikker <me@lenn4rt.com>
2019-05-21 14:49:20 +0200

    Made it possible to submit the form.

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require('express');
+var slug = require('slug');
+var bodyParser = require('body-parser');
 
 var userAvailability = true;
+var data = [];
 
 express()
     .set('view engine', 'ejs')
     .use(express.static(__dirname + '/static'))
+    .use(bodyParser.urlencoded({extended: true}))
     .get('/', home)
     .get('/profile', profile)
     .get('/information', information)
+    .post('/information', change)
     .get('/available', available)
     .get('*', pageNotFound)
     .listen(8080);
@@ -37,6 +42,24 @@ express()
             headerText: headerText,
             backLink: backLink
         });
+    }
+
+    function change(req, res) {
+        var name = slug(req.body.name);
+        data.push({
+            id: name.toLowerCase,
+            name: name,
+            birthday: req.body.birthday,
+            introduction: req.body.introduction,
+            music: req.body.music,
+            movies: req.body.movies,
+            books: req.body.books,
+            animal: req.body.animal,
+            dog_breed: req.body.dogBreed,
+            catBreed: req.body.catBreed
+        });
+        console.log(data[0]);
+        res.redirect('/profile');
     }
 
     function available(req, res) {
