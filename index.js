7332f021865531eb9677f88ac7bea8fb5b5b29ce
lennartdeknikker <me@lenn4rt.com>
2019-05-21 12:11:05 +0200

    changed the html to ejs templates

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-
-express()
-    .use(express.static('static'))
-    .use(bodyParser.urlencoded({extended: true}))
-    .set()
-    ;
