d44eebf1e83bd018c244794c21a7b1be94b65672
lennartdeknikker <me@lenn4rt.com>
2019-05-21 13:39:12 +0200

    Added partials for the headers.

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Dating-app-registration
-This feature can be used in dating apps for new users to register.
+This feature can be used in dating apps to change or add information to a profile.
 
 ## Installation
 Either through cloning with git or by using npm (the recommended way):
 
 `npm install dating-app-registration`
 
-And the feature will be installed =to your system path.
+And the feature will be installed to your system path.
 
-You can also install the registration-feature as a development dependency:
+You can also install this feature as a development dependency:
 
 `npm install --save-dev dating-app-registration`
--- server.js
+++ server.js
@@ -1,22 +1,51 @@
 var express = require('express');
 
+var userAvailability = true;
+
 express()
     .set('view engine', 'ejs')
     .use(express.static(__dirname + '/static'))
-    .get('/', function(req, res) {
-        res.render('pages/index');
-    })
-    .get('/profile', function(req, res) {
-        res.render('pages/profile');
-    })
-    .get('/information', function(req, res) {
-        res.render('pages/information');
-    })
-    .get('/available', function(req, res) {
-        res.render('pages/available');
-    })
-    .get('*', function(req, res){
-        res.send('The requested page does not exist.', 404);
-    })
+    .get('/', home)
+    .get('/profile', profile)
+    .get('/information', information)
+    .get('/available', available)
+    .get('*', pageNotFound)
     .listen(8080);
     console.log('listening on port 8080');
+
+
+    function home(req, res) {
+        userAvailability = true;
+        res.render('pages/index', {
+            userAvailability: userAvailability
+        });
+    }
+
+    function profile(req, res) {
+        var headerText = "My Profile";
+        var backLink = "/";
+        res.render('pages/profile', {
+            headerText: headerText,
+            backLink: backLink
+        });
+    }
+
+    function information(req, res) {
+        var headerText = "Change / Add Information";
+        var backLink = "/profile";
+        res.render('pages/information', {
+            headerText: headerText,
+            backLink: backLink
+        });
+    }
+
+    function available(req, res) {
+        userAvailability = false;
+        res.render('pages/available', {
+            userAvailability: userAvailability
+        });
+    }
+
+    function pageNotFound(req, res){
+        res.send('The requested page does not exist.', 404);
+    }
