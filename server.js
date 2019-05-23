86be9dbb9664ba6da7ba3af2dd939cc1c864771b
lennartdeknikker <me@lenn4rt.com>
2019-05-23 16:20:54 +0200

    started on setting up mongodb.

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var express = require('express');
 var slug = require('slug');
 var bodyParser = require('body-parser');
 var request = require('request');
+var mongo = require('mongodb');
 
 require('dotenv').config();
 
 // global variables
+var db = null;
+var url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT;
+
 var userAvailability = true;
 var idCounter = 0;
 var data = [{
@@ -22,6 +26,48 @@ var data = [{
     catBreed: undefined
 }];
 
+// mongodb setup
+/*
+mongo.MongoClient.connect(url, function (err, client) {
+    if (err) throw err;
+    db = client.db(process.env.DB_NAME);
+});
+
+function personal_details(req, res, next) {
+    db.collection('details').find().toArray(done);
+
+    function done(err, data) {
+        if (err) {
+            next(err);
+        } else {
+            res.render('list.ejs', {
+                data: data
+            });
+        }
+    }
+}
+
+function add(req, res, next) {
+    var name = slug(req.body.name);
+    data[0] = req.body;
+    data[0].id = String(idCounter++).padStart(4, "0");
+    if (data[0].animal === "dog") {
+        data[0].catBreed = "";
+    } else {
+        data[0].dogBreed = "";
+    }
+    db.collection('details').insertOne(data, done);
+
+    function done(err, data) {
+        if (err) {
+            next(err);
+        } else {
+            res.redirect('/information');
+        }
+    }
+}
+*/
+
 // express setup
 express()
     .set('view engine', 'ejs')
@@ -36,7 +82,9 @@ express()
     .post('/information', save)
     .get('/available', available)
     .get('*', pageNotFound)
-    .listen(8080, function() { console.log('listening on port 8080'); });
+    .listen(8080, function () {
+        console.log('listening on port 8080');
+    });
 
 // functions for rendering pages
 function home(req, res) {
@@ -45,6 +93,7 @@ function home(req, res) {
         userAvailability: userAvailability
     });
 }
+
 function profile(req, res) {
     var headerText = "My Profile";
     var backLink = "/";
@@ -53,6 +102,7 @@ function profile(req, res) {
         backLink: backLink
     });
 }
+
 function information(req, res) {
     var headerText = "Change / Add Information";
     var backLink = "/profile";
@@ -61,13 +111,13 @@ function information(req, res) {
     var dogBreeds = [];
     request('https://dog.ceo/api/breeds/list/all', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-        console.log('statusCode:', response && response.statusCode);
-        var dogBreedsJSON = JSON.parse(body).message;
-        for (var breed in dogBreedsJSON) {
-            dogBreeds.push(breed);
-        }
-    } else {
-        console.log('error', error, response && response.statusCode);
+            console.log('statusCode:', response && response.statusCode);
+            var dogBreedsJSON = JSON.parse(body).message;
+            for (var breed in dogBreedsJSON) {
+                dogBreeds.push(breed);
+            }
+        } else {
+            console.log('error', error, response && response.statusCode);
         }
     });
 
